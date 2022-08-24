0aa8e8785ffeeb7552add6f8b170523f0e9fd728
KhalilMinalang <minalang.khalil@gmail.com>
2022-08-24 14:35:45 +0800

    Completed: 058 Creating an _Ingredient_ Model

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,11 @@
+export class ingredient {
+  // how classes should made originally
+  // public name: string;
+  // public amount: number;
+  // constructor(name: string, amount: number) {
+  //   this.name = name;
+  //   this.amount = amount;
+  // }
+  // its' shortcut
+  constructor(public name: string, public amount: number) {}
+}
