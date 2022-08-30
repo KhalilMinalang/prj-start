191f1639cc2d9bb2a0462eef2790f3e6d6f3d40a
KhalilMinalang <minalang.khalil@gmail.com>
2022-08-30 15:04:19 +0800

    Completed: 118 Managing Recipes in a Recipe Service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,1 +1,22 @@
-export class RecipeService {}
+import { Recipe } from "./recipe.model";
+
+export class RecipeService {
+  // make it private
+  private recipes: Recipe[] = [
+    new Recipe(
+      "A Test Recipe",
+      "This is simply a test",
+      "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg"
+    ),
+    new Recipe(
+      "Another Test Recipe",
+      "This is simply a test",
+      "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg"
+    ),
+  ];
+
+  getRecipes() {
+    // this will simply return a new array which is an exact copy of the one in this service file.
+    return this.recipes.slice();
+  }
+}
