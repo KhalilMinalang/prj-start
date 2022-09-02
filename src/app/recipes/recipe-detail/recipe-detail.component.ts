e8a0197ac0927b27a4d2f01f838f3288cc4295f7
KhalilMinalang <minalang.khalil@gmail.com>
2022-09-02 19:40:06 +0800

    Completed: 162 Configuring Route Parameters

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from "@angular/core";
-import { ShoppingListService } from "src/app/shopping-list/shopping-list.service";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Params } from "@angular/router";
+
 import { Recipe } from "../recipe.model";
 import { RecipeService } from "../recipe.service";
 
@@ -9,14 +10,20 @@ import { RecipeService } from "../recipe.service";
   styleUrls: ["./recipe-detail.component.css"],
 })
 export class RecipeDetailComponent implements OnInit {
-  @Input() recipe: Recipe;
+  recipe: Recipe;
+  id: number;
 
   constructor(
     private recipeService: RecipeService,
-    private slService: ShoppingListService
+    private route: ActivatedRoute
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params["id"];
+      this.recipe = this.recipeService.getRecipe(this.id);
+    });
+  }
 
   onAddToShoppingList() {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
--- src/app/recipes/recipe-start/recipe-start.component.ts
+++ src/app/recipes/recipe-start/recipe-start.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Recipe } from "../recipe.model";
+import { RecipeService } from "../recipe.service";
 
 @Component({
-  selector: 'app-recipe-start',
-  templateUrl: './recipe-start.component.html',
-  styleUrls: ['./recipe-start.component.css']
+  selector: "app-recipe-start",
+  templateUrl: "./recipe-start.component.html",
+  styleUrls: ["./recipe-start.component.css"],
 })
 export class RecipeStartComponent implements OnInit {
+  selectedRecipe: Recipe;
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) {}
 
   ngOnInit(): void {
+    this.recipeService.recipeSelected.subscribe((recipe: Recipe) => {
+      this.selectedRecipe = recipe;
+      console.log(this.selectedRecipe);
+    });
   }
-
 }
