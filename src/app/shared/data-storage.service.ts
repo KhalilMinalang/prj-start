ab721d5f26fe16df922c48452c633a910df3a709
KhalilMinalang <minalang.khalil@gmail.com>
2022-09-09 13:51:45 +0800

    Completed: 282 Storing Recipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,7 +1,21 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { RecipeService } from "../recipes/recipe.service";
 
 @Injectable({ providedIn: "root" })
 export class DataStorageService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    console.log(recipes);
+    this.http
+      .put(
+        "https://ng-course-recipe-book-9aa82-default-rtdb.asia-southeast1.firebasedatabase.app/recipes.json",
+        recipes
+      )
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
 }
