4971cc15232187d2117a4c8e39d802255fac8958
KhalilMinalang <minalang.khalil@gmail.com>
2022-09-13 12:20:06 +0800

    Completed: 325 Adding Routes to Feature Modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,8 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
-import { RecipesComponent } from "./recipes/recipes.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
-import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
-import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
-import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
-import { RecipesResolverService } from "./recipes/recipes-resolver.service";
 import { AuthComponent } from "./auth/auth.component";
-import { AuthGuard } from "./auth/auth.guard";
 
 const appRoutes: Routes = [
   {
@@ -17,32 +11,7 @@ const appRoutes: Routes = [
     // please provide 'pathMatch' error fix
     pathMatch: "full",
   },
-  {
-    path: "recipes",
-    component: RecipesComponent,
-    canActivate: [AuthGuard],
-    children: [
-      {
-        path: "",
-        component: RecipeStartComponent,
-      },
-      {
-        path: "new",
-        component: RecipeEditComponent,
-      },
-      // routes with dynamic parameters must be in bottom
-      {
-        path: ":id",
-        component: RecipeDetailComponent,
-        resolve: [RecipesResolverService],
-      },
-      {
-        path: ":id/edit",
-        component: RecipeEditComponent,
-        resolve: [RecipesResolverService],
-      },
-    ],
-  },
+
   {
     path: "shopping-list",
     component: ShoppingListComponent,
