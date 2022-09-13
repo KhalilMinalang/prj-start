e39c06b12589d3a479ffc8562761ee061df9e939
KhalilMinalang <minalang.khalil@gmail.com>
2022-09-13 11:39:43 +0800

    Completed: 324 Splitting Modules Correctly

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -1,4 +1,7 @@
 import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
+import { ReactiveFormsModule } from "@angular/forms";
 
 import { RecipesComponent } from "./recipes.component";
 import { RecipeListComponent } from "./recipe-list/recipe-list.component";
@@ -16,6 +19,8 @@ import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
     RecipeStartComponent,
     RecipeEditComponent,
   ],
+  // solution 1
+  imports: [RouterModule, CommonModule, ReactiveFormsModule],
   exports: [
     RecipesComponent,
     RecipeListComponent,
