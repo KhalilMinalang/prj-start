bd2b96650bd9ba2d58d73fbd68a9b39c1b1016ab
KhalilMinalang <minalang.khalil@gmail.com>
2022-09-13 15:30:30 +0800

    Completed: 327 The ShoppingList Feature Module

---
+++ src/app/shopping-list/shopping-list-routing.module.ts
@@ -0,0 +1,17 @@
+// it's also possible to shortcut this instead since there's only 1 route
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { ShoppingListComponent } from "./shopping-list.component";
+
+const routes: Routes = [
+  {
+    path: "shopping-list",
+    component: ShoppingListComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class ShoppingListRoutingModule {}
