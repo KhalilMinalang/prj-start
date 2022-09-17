f1b7f7644f1c7494587b8b12fc457a942bc7a332
KhalilMinalang <minalang.khalil@gmail.com>
2022-09-17 22:18:52 +0800

    Completed: 380 Managing UI State in NgRx

--- src/app/auth/store/auth.actions.ts
+++ src/app/auth/store/auth.actions.ts
@@ -2,6 +2,7 @@ import { Action } from "@ngrx/store";
 
 export const LOGIN_START = "[Auth] Login Start";
 export const LOGIN = "[Auth] Login";
+export const LOGIN_FAIL = "[Auth] Login Fail";
 export const LOGOUT = "[Auth] Logout";
 
 export class Login implements Action {
@@ -24,9 +25,13 @@ export class Logout implements Action {
 export class LoginStart implements Action {
   readonly type = LOGIN_START;
 
-  constructor(public payload: { email: string; password: string }) {
-    console.log(LOGIN_START, "test");
-  }
+  constructor(public payload: { email: string; password: string }) {}
 }
 
-export type AuthActions = Login | Logout | LoginStart;
+export class LoginFail implements Action {
+  readonly type = LOGIN_FAIL;
+
+  constructor(public payload: string) {}
+}
+
+export type AuthActions = Login | Logout | LoginStart | LoginFail;
