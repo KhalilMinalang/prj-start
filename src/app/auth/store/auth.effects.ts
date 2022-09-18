a2f7353bb153c3f1bf06f5c69193a0e258d61c27
KhalilMinalang <minalang.khalil@gmail.com>
2022-09-18 11:55:17 +0800

    Completed: 385 Adding Auto-Login with NgRx

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -7,6 +7,7 @@ import { HttpClient } from "@angular/common/http";
 import { environment } from "src/environments/environment";
 
 import * as AuthActions from "./auth.actions";
+import { User } from "../user.model";
 
 export interface AuthResponsData {
   kind: string;
@@ -25,6 +26,8 @@ const handleAuthentication = (
   token: string
 ) => {
   const expirationDate = new Date(new Date().getTime() + +expiresIn * 1000);
+  const user = new User(email, userId, token, expirationDate);
+  localStorage.setItem("userData", JSON.stringify(user));
   return new AuthActions.AuthenticateSuccess({
     email: email,
     userId: userId,
@@ -129,6 +132,52 @@ export class AuthEffects {
     ),
     tap(() => {
       this.router.navigate(["/"]);
+    })
+  );
+
+  @Effect()
+  autoLogin = this.actions$.pipe(
+    ofType(AuthActions.AUTO_LOGIN),
+    map(() => {
+      const userData: {
+        email: string;
+        id: string;
+        _token: string;
+        _tokenExpirationDate: Date;
+      } = JSON.parse(localStorage.getItem("userData"));
+      if (!userData) {
+        return { type: "DUMMY" };
+      }
+
+      const loadedUser = new User(
+        userData.email,
+        userData.id,
+        userData._token,
+        new Date(userData._tokenExpirationDate)
+      );
+
+      if (loadedUser.token) {
+        // this.user.next(loadedUser);
+        return new AuthActions.AuthenticateSuccess({
+          email: loadedUser.email,
+          userId: loadedUser.id,
+          token: loadedUser.token,
+          expirationDate: new Date(userData._tokenExpirationDate),
+        });
+        // const expirationDuration =
+        //   new Date(userData._tokenExpirationDate).getTime() -
+        //   new Date().getTime();
+        // this.autoLogout(expirationDuration);
+      }
+      return { type: "DUMMY" };
+    })
+  );
+
+  @Effect({ dispatch: false })
+  authLogout = this.actions$.pipe(
+    ofType(AuthActions.LOGOUT),
+    tap(() => {
+      localStorage.removeItem("userData");
     })
   );
 
