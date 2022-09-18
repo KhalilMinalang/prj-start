fdfd387b0b16877e9bf185b24a4c9d18d52327c3
KhalilMinalang <minalang.khalil@gmail.com>
2022-09-18 17:45:43 +0800

    Completed: 388 Using the Store Devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,19 @@
 import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { StoreModule } from "@ngrx/store";
+import { EffectsModule } from "@ngrx/effects";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { AppRoutingModule } from "./app-routing.module";
 import { SharedModule } from "./shared/shared.module";
 import { CoreModule } from "./core.module";
-import { LogginService } from "./logging.service";
-import { StoreModule } from "@ngrx/store";
-import { EffectsModule } from "@ngrx/effects";
-// import { shoppingListReducer } from "./shopping-list/store/shopping-list.reducer";
-// import { authReducer } from "./auth/store/auth.reducer";
 import * as fromApp from "./store/app.reducer";
+import { LogginService } from "./logging.service";
 import { AuthEffects } from "./auth/store/auth.effects";
+import { environment } from "src/environments/environment";
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
@@ -21,12 +21,9 @@ import { AuthEffects } from "./auth/store/auth.effects";
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    // StoreModule.forRoot({
-    //   shoppingList: shoppingListReducer,
-    //   auth: authReducer,
-    // }),
     StoreModule.forRoot(fromApp.appReducer),
     EffectsModule.forRoot([AuthEffects]),
+    StoreDevtoolsModule.instrument({ logOnly: environment.production }),
     SharedModule,
     CoreModule,
   ],
--- src/app/auth/store/auth.actions.ts
+++ src/app/auth/store/auth.actions.ts
@@ -1,8 +1,8 @@
 import { Action } from "@ngrx/store";
 
 export const LOGIN_START = "[Auth] Login Start";
-export const AUTHENTICATE_SUCCESS = "[Auth] Authenticate Success";
-export const AUTHENTICATE_FAIL = "[Auth] Authenticate Fail";
+export const AUTHENTICATE_SUCCESS = "[Auth] Login";
+export const AUTHENTICATE_FAIL = "[Auth] Login Fail";
 export const SIGNUP_START = "[Auth] Signup Start";
 export const CLEAR_ERROR = "[Auth] Clear Error";
 export const AUTO_LOGIN = "[Auth] Auto Login";
