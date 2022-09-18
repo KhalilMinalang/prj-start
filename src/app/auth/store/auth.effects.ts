4b6f757fbecc63a91bb818573ef314dfc571d2b2
KhalilMinalang <minalang.khalil@gmail.com>
2022-09-18 10:10:34 +0800

    Completed: 383 Adding Signup

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -18,9 +18,74 @@ export interface AuthResponsData {
   registered?: boolean;
 }
 
+const handleAuthentication = (
+  expiresIn: number,
+  email: string,
+  userId: string,
+  token: string
+) => {
+  const expirationDate = new Date(new Date().getTime() + +expiresIn * 1000);
+  return new AuthActions.AuthenticateSuccess({
+    email: email,
+    userId: userId,
+    token: token,
+    expirationDate: expirationDate,
+  });
+};
+const handleError = (errorRes: any) => {
+  let errorMessage = "An unknown error occurred!";
+  if (!errorRes.error || !errorRes.error.error) {
+    // of() returns an argument as observable
+    return of(new AuthActions.AuthenticateFail(errorMessage));
+  }
+  switch (errorRes.error.error.message) {
+    case "EMAIL_EXISTS":
+      errorMessage = "This email exists already";
+      break;
+    case "EMAIL_NOT_FOUND":
+      errorMessage = "This email does not exist.";
+      break;
+    case "INVALID_PASSWORD":
+      errorMessage = "This password is not correct";
+      break;
+  }
+  return of(new AuthActions.AuthenticateFail(errorMessage));
+};
+
 @Injectable()
 export class AuthEffects {
-  authSignup = this.actions$.pipe(ofType(AuthActions.SIGNUP_START));
+  @Effect()
+  authSignup = this.actions$.pipe(
+    ofType(AuthActions.SIGNUP_START),
+    switchMap((signupAction: AuthActions.SignupStart) => {
+      console.log("SignupStart call effect");
+      return this.http
+        .post<AuthResponsData>(
+          "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" +
+            environment.firebaseAPIKey,
+          {
+            email: signupAction.payload.email,
+            password: signupAction.payload.password,
+            returnSecureToken: true,
+          }
+        )
+        .pipe(
+          map((resData) => {
+            console.log("ok");
+            return handleAuthentication(
+              +resData.expiresIn,
+              resData.email,
+              resData.localId,
+              resData.idToken
+            );
+          }),
+          catchError((errorRes) => {
+            console.log("err");
+            return handleError(errorRes);
+          })
+        );
+    })
+  );
 
   @Effect()
   authLogin = this.actions$.pipe(
@@ -40,34 +105,15 @@ export class AuthEffects {
           // must pipe here instead so this effect won't die
           .pipe(
             map((resData) => {
-              const expirationDate = new Date(
-                new Date().getTime() + +resData.expiresIn * 1000
+              return handleAuthentication(
+                +resData.expiresIn,
+                resData.email,
+                resData.localId,
+                resData.idToken
               );
-              return new AuthActions.AuthenticateSuccess({
-                email: resData.email,
-                userId: resData.localId,
-                token: resData.idToken,
-                expirationDate: expirationDate,
-              });
             }),
             catchError((errorRes) => {
-              let errorMessage = "An unknown error occurred!";
-              if (!errorRes.error || !errorRes.error.error) {
-                // of() returns an argument as observable
-                return of(new AuthActions.AuthenticateFail(errorMessage));
-              }
-              switch (errorRes.error.error.message) {
-                case "EMAIL_EXISTS":
-                  errorMessage = "This email exists already";
-                  break;
-                case "EMAIL_NOT_FOUND":
-                  errorMessage = "This email does not exist.";
-                  break;
-                case "INVALID_PASSWORD":
-                  errorMessage = "This password is not correct";
-                  break;
-              }
-              return of(new AuthActions.AuthenticateFail(errorMessage));
+              return handleError(errorRes);
             })
           )
       );
